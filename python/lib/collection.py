bdb8881a29416bfce6e20dfb2a2bcf459888c98a
Marcus <marcusposey@gmail.com>
2017-07-23 12:36:40 -0500

    Make table name and connection members "private"

--- python/lib/collection.py
+++ python/lib/collection.py
@@ -5,16 +5,16 @@ class Collection():
     """Collection manages local comics and ranking information."""
 
     def __init__(self, file_path):
-        self.comics_table = "comics"
-        self.words_table = "words"
-        self.word_weights_table = "word_weights"
+        self.__comics_table = "comics"
+        self.__words_table = "words"
+        self.__word_weights_table = "word_weights"
 
-        self.con = self.__connect(file_path)
-        self.cursor = self.con.cursor()
+        self.__con = self.__connect(file_path)
+        self.__cursor = self.__con.cursor()
 
     def __del__(self):
-        self.con.commit()
-        self.con.close()
+        self.__con.commit()
+        self.__con.close()
 
     def __connect(self, file_path):
         is_new_collection = not os.path.isfile(file_path)
@@ -28,13 +28,13 @@ class Collection():
                         title TEXT NOT NULL,\
                         alt TEXT NOT NULL,\
                         transcript TEXT NOT NULL\
-                        )'.format(self.comics_table))
+                        )'.format(self.__comics_table))
 
             c.execute('CREATE TABLE {} (\
                         id INTEGER PRIMARY KEY AUTOINCREMENT,\
                         word TEXT NOT NULL UNIQUE,\
                         is_blacklisted INTEGER DEFAULT 0 CHECK(is_blacklisted in (0,1))\
-                        )'.format(self.words_table))
+                        )'.format(self.__words_table))
 
             c.execute('CREATE TABLE {} (\
                         word_id INTEGER,\
@@ -43,7 +43,7 @@ class Collection():
                         PRIMARY KEY (word_id, comic_id),\
                         FOREIGN KEY (word_id) REFERENCES {}(id),\
                         FOREIGN KEY (comic_id) REFERENCES {}(id)\
-                        )'.format(self.word_weights_table, self.words_table, self.comics_table))
+                        )'.format(self.__word_weights_table, self.__words_table, self.__comics_table))
             con.commit()
 
         return con
@@ -60,8 +60,8 @@ class Collection():
                 "transcript": string
             }
         """
-        self.cursor.execute("INSERT INTO {} (id, img_url, title, alt, transcript) VALUES ({},'{}','{}','{}','{}')"
-                            .format(self.comics_table, comic["number"], comic["img_url"], comic["title"],
+        self.__cursor.execute("INSERT INTO {} (id, img_url, title, alt, transcript) VALUES ({},'{}','{}','{}','{}')"
+                              .format(self.__comics_table, comic["number"], comic["img_url"], comic["title"],
                                     comic["alt"], comic["transcript"]))
 
         phrase = '{} {} {}'.format(comic["title"], comic["alt"], comic["transcript"]).split(' ')
@@ -70,30 +70,30 @@ class Collection():
             word = word.lower()
             # Add the word to the database if it isn't already there.
 
-            self.cursor.execute("SELECT id FROM {} WHERE word = '{}'".format(self.words_table, word))
+            self.__cursor.execute("SELECT id FROM {} WHERE word = '{}'".format(self.__words_table, word))
 
-            res = self.cursor.fetchone()
+            res = self.__cursor.fetchone()
             word_id = 0
             if res is None:
-                self.cursor.execute("INSERT INTO {} (word) VALUES ('{}')".format(self.words_table, word))
-                word_id = self.cursor.lastrowid
+                self.__cursor.execute("INSERT INTO {} (word) VALUES ('{}')".format(self.__words_table, word))
+                word_id = self.__cursor.lastrowid
             else:
                 word_id = res[0]
 
             # Update the weight of the word for this comic.
 
-            self.cursor.execute('SELECT weight FROM {} WHERE word_id={} AND comic_id={}'
-                                .format(self.word_weights_table, word_id, comic["number"]))
-            res = self.cursor.fetchone()
+            self.__cursor.execute('SELECT weight FROM {} WHERE word_id={} AND comic_id={}'
+                                  .format(self.__word_weights_table, word_id, comic["number"]))
+            res = self.__cursor.fetchone()
 
             if res is None:
-                self.cursor.execute('INSERT INTO {} (word_id, comic_id) VALUES ({}, {})'
-                                    .format(self.word_weights_table, word_id, comic["number"]))
+                self.__cursor.execute('INSERT INTO {} (word_id, comic_id) VALUES ({}, {})'
+                                      .format(self.__word_weights_table, word_id, comic["number"]))
             else:
-                self.cursor.execute('UPDATE {} SET weight={} WHERE word_id={} AND comic_id={}'
-                                    .format(self.word_weights_table, res[0] + 1, word_id, comic["number"]))
+                self.__cursor.execute('UPDATE {} SET weight={} WHERE word_id={} AND comic_id={}'
+                                      .format(self.__word_weights_table, res[0] + 1, word_id, comic["number"]))
 
-        self.con.commit()
+        self.__con.commit()
 
     def add_to_blacklist(self, word):
         """Add a word to the list of blacklisted words.
@@ -105,13 +105,13 @@ class Collection():
         """
         word = word.lower()
         try:
-            self.cursor.execute("INSERT INTO {} (word, is_blacklisted) VALUES ('{}', {})"
-                            .format(self.words_table, word, 1))
+            self.__cursor.execute("INSERT INTO {} (word, is_blacklisted) VALUES ('{}', {})"
+                                  .format(self.__words_table, word, 1))
         except:
-            self.cursor.execute("UPDATE {} SET is_blacklisted={} WHERE word='{}'"
-                                .format(self.words_table, 1, word))
+            self.__cursor.execute("UPDATE {} SET is_blacklisted={} WHERE word='{}'"
+                                  .format(self.__words_table, 1, word))
 
-        self.con.commit()
+        self.__con.commit()
 
     def get_comic(self, number):
         """Return a comic from the collection.
@@ -121,8 +121,8 @@ class Collection():
         :param number: An integer that uniquely identifies the comic
         :return: Either the comic matching the number or None if it is not in the collection
         """
-        self.cursor.execute('SELECT * FROM {} WHERE id = {}'.format(self.comics_table, number))
-        res = self.cursor.fetchone()
+        self.__cursor.execute('SELECT * FROM {} WHERE id = {}'.format(self.__comics_table, number))
+        res = self.__cursor.fetchone()
 
         if res is None:
             return None
@@ -146,15 +146,15 @@ class Collection():
         # Find the comics that mention words in phrase.
         for word in phrase:
             word = word.lower()
-            self.cursor.execute("SELECT id FROM {} WHERE word='{}' AND is_blacklisted=0"
-                                .format(self.words_table, word))
-            res = self.cursor.fetchone()
+            self.__cursor.execute("SELECT id FROM {} WHERE word='{}' AND is_blacklisted=0"
+                                  .format(self.__words_table, word))
+            res = self.__cursor.fetchone()
             if res is None:
                 continue
 
-            self.cursor.execute('SELECT comic_id, weight FROM {} WHERE word_id={}'
-                                .format(self.word_weights_table, res[0]))
-            for row in self.cursor.fetchall():
+            self.__cursor.execute('SELECT comic_id, weight FROM {} WHERE word_id={}'
+                                  .format(self.__word_weights_table, res[0]))
+            for row in self.__cursor.fetchall():
                 comic_id = row[0]
                 weight = row[1]
                 if comic_id in comics:
